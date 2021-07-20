12fdc041c997d3a9cfa7e048ce96aa35be19f68c
buthed010203 <naguiar010203@gmail.com>
2021-07-20 11:18:57 -0400

    Fix the stupid logging for real?

--- src/SteamServer.java
+++ src/SteamServer.java
@@ -53,14 +53,17 @@ public class SteamServer extends Plugin{
         boolean[] isShutdown = {false};
 
         Events.on(EventType.DisposeEvent.class, event -> {
+            SVars.net.closeServer();
+            Threads.sleep(100);
             SteamAPI.shutdown();
             isShutdown[0] = true;
         });
 
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             if (!isShutdown[0]) {
-                SteamAPI.shutdown();
+                SVars.net.closeServer();
                 Threads.sleep(100);
+                SteamAPI.shutdown();
             }
         }));
     }
